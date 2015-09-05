9673e5a61186e352fb8c1a08842b8678d26aef18
nickpiscopio <nick.piscopio@gmail.com>
2015-09-05 15:33:30 -0400

    Commented out the upgrade code.
    Changed the version number.

--- app/src/main/java/com/catalyst/catalyst/receiver/UpgradeReceiver.java
+++ app/src/main/java/com/catalyst/catalyst/receiver/UpgradeReceiver.java
@@ -3,10 +3,6 @@ package com.catalyst.catalyst.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
-
-import com.catalyst.catalyst.util.Constant;
 
 /**
  * Listens for the app to be upgraded.
@@ -18,14 +14,14 @@ public class UpgradeReceiver extends BroadcastReceiver
     @Override
     public void onReceive(Context context, Intent intent)
     {
-        Uri packageName = intent.getData();
-
-        if(packageName.toString().equals("package:" + context.getPackageName()))
-        {
-            SharedPreferences prefs = context.getSharedPreferences(Constant.SHARED_PREFERENCES, Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = prefs.edit();
-            editor.clear();
-            editor.apply();
-        }
+//        Uri packageName = intent.getData();
+//
+//        if(packageName.toString().equals("package:" + context.getPackageName()))
+//        {
+//            SharedPreferences prefs = context.getSharedPreferences(Constant.SHARED_PREFERENCES, Context.MODE_PRIVATE);
+//            SharedPreferences.Editor editor = prefs.edit();
+//            editor.clear();
+//            editor.apply();
+//        }
     }
 }
